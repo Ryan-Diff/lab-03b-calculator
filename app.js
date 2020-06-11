9dd251932d2024921cefeb882447e98ad75a0035
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-06-10 17:55:33 -0700

    -m 'Trying to upload calc'

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 const addButton = document.getElementById('sum-button');
 
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener('click' , () => {
     const input1 = document.getElementById('add-one');
     const input2 = document.getElementById('add-two');
+    
+    console.log(input1.value);
+    console.log(input2.value);
+
+    console.log(Number(input1.value) + Number(input2.value));
+
     const resultSpan = document.getElementById('sum-result');
-
-    const firstValue = Number(input1.value);
-    const secondValue = Number(input2.value);
-    const sum = firstValue + secondValue;
-
-    resultSpan.textContent = sum;
-});
-
-const subButton = document.getElementById('sub-button');
-
-
-subButton.addEventListener('click', () => {
-    const input1 = document.getElementById('add-one');
-    const input2 = document.getElementById('add-two');
-    const resultSpan = document.getElementById('sub-result');
-
-    const firstValue = Number(input1.value);
-    const secondValue = Number(input2.value);
-    const sum = firstValue - secondValue;
-
-    resultSpan.textContent = -sum;
-});
+    resultSpan.textContent = Number(input1.value) + Number(input2.value);
+})
