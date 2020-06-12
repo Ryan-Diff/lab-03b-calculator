ee7229536a2802fd59ffd79aca52cc5d5b7a325f
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-06-11 17:15:42 -0700

    Functional Calculator w/ Tests

--- app.js
+++ app.js
@@ -1,50 +1,39 @@
-const addButton = document.getElementById('sum-button');
+import { addtest, subtest, multtest, divtest } from "./test/utils.js";
 
+const addButton = document.getElementById('sum-button');
+const subButton = document.getElementById('sub-button');
+const multButton = document.getElementById('mult-button');
+const divButton = document.getElementById('div-button');
+const input1 = document.getElementById('add-one');
+const input2 = document.getElementById('add-two');
+const input3 = document.getElementById('sub-one');
+const input4 = document.getElementById('sub-two');
+const input5 = document.getElementById('mult-one');
+const input6 = document.getElementById('mult-two');
+const input7 = document.getElementById('div-one');
+const input8 = document.getElementById('div-two');
 
 addButton.addEventListener('click' , () => {
-    const input1 = document.getElementById('add-one');
-    const input2 = document.getElementById('add-two');
-    
-   
     const resultSpan = document.getElementById('sum-result');
-    resultSpan.textContent = Number(input1.value) + Number(input2.value);
+    // resultSpan.textContent = Number(input1.value) - Number(input2.value);
+    resultSpan.textContent = addtest(Number(input1.value) , Number(input2.value));
 })
-
-const subButton = document.getElementById('sub-button');
 
 
 subButton.addEventListener('click' , () => {
-    const input3 = document.getElementById('sub-one');
-    const input4 = document.getElementById('sub-two');
-    
-    
-
     const resultSpan = document.getElementById('sub-result');
-    resultSpan.textContent = Number(input3.value) - Number(input4.value);
+    // resultSpan.textContent = Number(input3.value) - Number(input4.value);
+    resultSpan.textContent = subtest(Number(input3.value) , Number(input4.value));
 })
-
-const multButton = document.getElementById('mult-button');
-
 
 multButton.addEventListener('click' , () => {
-    const input5 = document.getElementById('mult-one');
-    const input6 = document.getElementById('mult-two');
-    
-    
-
     const resultSpan = document.getElementById('mult-result');
-    resultSpan.textContent = Number(input5.value) * Number(input6.value);
+    // resultSpan.textContent = Number(input5.value) * Number(input6.value);
+    resultSpan.textContent = multtest(Number(input5.value) , Number(input6.value));
 })
 
-const divButton = document.getElementById('div-button');
-
-
 divButton.addEventListener('click' , () => {
-    const input7 = document.getElementById('div-one');
-    const input8 = document.getElementById('div-two');
-    
-    
-
     const resultSpan = document.getElementById('div-result');
-    resultSpan.textContent = Number(input7.value) / Number(input8.value);
+    // resultSpan.textContent = Number(input7.value) / Number(input8.value);
+    resultSpan.textContent = divtest(Number(input7.value) , Number(input8.value));
 })
--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,8 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import {addtest, subtest, multtest, divtest} from './utils.js'
+
+
 
 const test = QUnit.test;
 
@@ -16,3 +19,108 @@ test('time to test a function', function(expect) {
     // Make assertions about what is expected versus the actual result
     expect.equal(expected, actual);
 });
+
+test('addtest', function(expect) {
+    const firstnum = 2;
+    const secondnum = 4;
+    const expected = 6;
+    const actual = addtest(firstnum, secondnum);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(expected, actual);
+});
+
+test('addtest', function(expect) {
+    const firstnum = 5;
+    const secondnum = 3;
+    const expected = 8;
+    const actual = addtest(firstnum, secondnum);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(expected, actual);
+});
+
+test('addtest', function(expect) {
+    const firstnum = 23;
+    const secondnum = 7;
+    const expected = 30;
+    const actual = addtest(firstnum, secondnum);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(expected, actual);
+});
+
+test('subtest', function(expect) {
+    const firstnum = 5
+    const secondnum = 3
+    const expected = 2
+    const actual = subtest(firstnum, secondnum);
+    expect.equal(expected, actual);
+})
+
+test('subtest', function(expect) {
+    const firstnum = 11
+    const secondnum = 11
+    const expected = 0
+    const actual = subtest(firstnum, secondnum);
+    expect.equal(expected, actual);
+})
+
+test('subtest', function(expect) {
+    const firstnum = -1
+    const secondnum = 1
+    const expected = -2
+    const actual = subtest(firstnum, secondnum);
+    expect.equal(expected, actual);
+})
+
+test('multtest', function(expect) {
+    const firstnum = 3
+    const secondnum = 3
+    const expected = 9
+    const actual = multtest(firstnum, secondnum);
+    expect.equal(expected, actual);
+})
+
+test('multtest', function(expect) {
+    const firstnum = 21
+    const secondnum = 21
+    const expected = 441
+    const actual = multtest(firstnum, secondnum);
+    expect.equal(expected, actual);
+})
+
+test('multtest', function(expect) {
+    const firstnum = -3
+    const secondnum = 3
+    const expected = -9
+    const actual = multtest(firstnum, secondnum);
+    expect.equal(expected, actual);
+})
+
+    test('divtest', function(expect) {
+        const firstnum = 5
+        const secondnum = 1
+        const expected = 5
+        const actual = divtest(firstnum, secondnum);
+        expect.equal(expected, actual);
+    })
+
+    test('divtest', function(expect) {
+        const firstnum = -25    
+        const secondnum = 5
+        const expected = -5
+        const actual = divtest(firstnum, secondnum);
+        expect.equal(expected, actual);
+    })
+
+    test('divtest', function(expect) {
+        const firstnum = 100
+        const secondnum = 20
+        const expected = 5
+        const actual = divtest(firstnum, secondnum);
+        expect.equal(expected, actual);
+    })
