b3f8d718355647edb8facb171b06749aab9b8e70
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-06-12 12:45:13 -0700

    -m 'Calc Test 6'

--- test/utils.js
+++ test/utils.js
@@ -1,16 +1,16 @@
-export function addtest(input1 , input2) {
-    return input1 + input2
+export function addtest(input1, input2) {
+    return input1 + input2;
 }
 
-export function subtest(input3 , input4) {
-    return input3 - input4
+export function subtest(input3, input4) {
+    return input3 - input4;
 }
 
-export function multtest(input5 , input6) {
-    return input5 * input6
+export function multtest(input5, input6) {
+    return input5 * input6;
 }
 
-export function divtest(input7 , input8)  {
-    return input7 / input8
+export function divtest(input7, input8) {
+    return input7 / input8;
 }       
 
