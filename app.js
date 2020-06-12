fa1943cfc3514490666f53422ac55a4a1e1374dd
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-06-11 17:32:41 -0700

    Functional Calculator w/ Tests pt 3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { addtest, subtest, multtest, divtest } from "./test/utils.js";
+import { addtest, subtest, multtest, divtest } from './test/utils.js';
 
 const addButton = document.getElementById('sum-button');
 const subButton = document.getElementById('sub-button');
@@ -13,27 +13,27 @@ const input6 = document.getElementById('mult-two');
 const input7 = document.getElementById('div-one');
 const input8 = document.getElementById('div-two');
 
-addButton.addEventListener('click' , () => {
+addButton.addEventListener('click', () => {
     const resultSpan = document.getElementById('sum-result');
     // resultSpan.textContent = Number(input1.value) - Number(input2.value);
-    resultSpan.textContent = addtest(Number(input1.value) , Number(input2.value));
-})
+    resultSpan.textContent = addtest(Number(input1.value), Number(input2.value));
+});
 
 
-subButton.addEventListener('click' , () => {
+subButton.addEventListener('click', () => {
     const resultSpan = document.getElementById('sub-result');
     // resultSpan.textContent = Number(input3.value) - Number(input4.value);
-    resultSpan.textContent = subtest(Number(input3.value) , Number(input4.value));
-})
+    resultSpan.textContent = subtest(Number(input3.value), Number(input4.value));
+});
 
-multButton.addEventListener('click' , () => {
+multButton.addEventListener('click', () => {
     const resultSpan = document.getElementById('mult-result');
     // resultSpan.textContent = Number(input5.value) * Number(input6.value);
-    resultSpan.textContent = multtest(Number(input5.value) , Number(input6.value));
-})
+    resultSpan.textContent = multtest(Number(input5.value), Number(input6.value));
+});
 
-divButton.addEventListener('click' , () => {
+divButton.addEventListener('click', () => {
     const resultSpan = document.getElementById('div-result');
     // resultSpan.textContent = Number(input7.value) / Number(input8.value);
-    resultSpan.textContent = divtest(Number(input7.value) , Number(input8.value));
-})
+    resultSpan.textContent = divtest(Number(input7.value), Number(input8.value));
+});
